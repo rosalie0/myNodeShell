d8cbb02a377eac6a9e50e3ffda293e17aa7c56c1
Rosalie Newman <rosalienewman0@gmail.com>
2022-09-08 15:36:16 -0400

    wrote 'date'. refactored all the if-else ifs in bash.js to be more readable.

--- bash.js
+++ bash.js
@@ -1,26 +1,32 @@
 const printDirectory = require('./pwd');
 const listFiles = require('./ls');
 const printFileContents = require('./cat');
-
+const printDate = require('./date');
 //Output a prompt
 process.stdout.write('prompt > ');
 
 const done = (output) => {
 	process.stdout.write(output);
-	process.stdout.write('\nprompt Again! > ');
+	process.stdout.write('\nprompt > ');
 };
+
+// An array of the Commands we've written to handle, so far.
+const commands = ['pwd', 'ls', 'cat', 'date'];
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
 	const cmd = data.toString().trim(); //remove the newline
-	const cmdSplitBySpaces = cmd.split(' '); // used for handling when input is multiple 'words'
+	const args = cmd.split(' '); // used for handling when input is multiple 'words'
 
-	if (cmd === 'pwd') {
-		printDirectory(done);
-	} else if (cmd === 'ls') {
-		listFiles(done);
-	} else if (cmdSplitBySpaces[0] === 'cat') {
-		const filename = cmdSplitBySpaces[1];
-		printFileContents(done, filename);
-	} else done('You typed: ' + cmd);
+	if (commands.includes(args[0])) {
+		if (args[0] === 'pwd') printDirectory(done);
+		if (args[0] === 'ls') listFiles(done);
+		if (args[0] === 'cat') {
+			const filename = args[1];
+			printFileContents(done, filename);
+		}
+		if (args[0] === 'date') printDate(done);
+	}
+	// Default. Just print back what user typed.
+	else done('You typed: ' + cmd);
 });
---
+++ date.js
@@ -0,0 +1,4 @@
+module.exports = function (done) {
+	const date = Date();
+	done(date);
+};
