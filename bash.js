ffc6a834622db4244109acca13433d4841f7cc29
Rosalie Newman <rosalienewman0@gmail.com>
2022-09-08 15:50:30 -0400

    about to refactor cmd and args in bash.js

--- bash.js
+++ bash.js
@@ -2,6 +2,7 @@ const printDirectory = require('./pwd');
 const listFiles = require('./ls');
 const printFileContents = require('./cat');
 const printDate = require('./date');
+const echo = require('./echo');
 //Output a prompt
 process.stdout.write('prompt > ');
 
@@ -11,12 +12,12 @@ const done = (output) => {
 };
 
 // An array of the Commands we've written to handle, so far.
-const commands = ['pwd', 'ls', 'cat', 'date'];
+const commands = ['pwd', 'ls', 'cat', 'date', 'echo'];
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
 	const cmd = data.toString().trim(); //remove the newline
-	const args = cmd.split(' '); // used for handling when input is multiple 'words'
+	const args = cmd.split(' ');
 
 	if (commands.includes(args[0])) {
 		if (args[0] === 'pwd') printDirectory(done);
@@ -26,7 +27,10 @@ process.stdin.on('data', (data) => {
 			printFileContents(done, filename);
 		}
 		if (args[0] === 'date') printDate(done);
-	}
-	// Default. Just print back what user typed.
-	else done('You typed: ' + cmd);
+		if (args[0] === 'echo') {
+			args.shift(); // Delete first element, in this case, 'echo'
+			const restOfString = args.join(' ');
+			echo(done, restOfString);
+		}
+	} else done(`Command not found: ${args[0]}`);
 });
